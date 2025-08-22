2a48d81f33bffad30a6b65f12c4ed145bff34d15
Harol Mauricio Gómez Zapata <harolmgzapata@gmail.com>
2025-08-21 21:12:47 -0500

    agregando usuarios

--- apphmgz/views.py
+++ apphmgz/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from .models import Usuarios
+from random import randint
 
 # Create your views here.
 
 def index(request):
+    usuario = Usuarios()
+    usr = str(randint(1000, 9999))
+    correo = f"{usr}@correo.com"
+    usuario.usuario = usr
+    usuario.correo = correo
+    usuario.clave = '1234'
+    usuario.save()
     usuarios = Usuarios.objects.all()
     contexto = {'usuarios': usuarios}
     return render(request, 'render/index.html', contexto)
